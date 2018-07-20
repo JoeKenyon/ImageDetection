adcdd4e9a5b83322428d0376a1d4e6899d200e41
JoeBoiii72 <joekenyon72@gmail.com>
2018-07-20 20:36:51 +0100

    Update example.py

--- example/example.py
+++ example/example.py
@@ -1,5 +1,5 @@
-from PyImage import PyDetector
+from ImageDetection import ImageDetect
 
-coords = PyDetector.DetectImage(True,0.8,"chars.JPG")
+coords = ImageDetect.DetectImage(True,0.8,"chars.JPG")
 
-PyDetector.ShowLocations(True,coords)
+ImageDetect.ShowLocations(True,coords)
